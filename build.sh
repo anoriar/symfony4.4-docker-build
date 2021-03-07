44d57ec0028da6eb3cd0744cbb680bf9349ab966
Alex Borisov <borisov@aeroidea.ru>
2021-03-07 23:44:02 +0300

    init2

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+UID=`id -u`
+GID=`id -g`
+cp -n .env.workspace .env
+cp -n  src/.env.workspace src/.env
+docker-compose down
+sudo rm -rf ./.docker/data/mysql
+sudo rm -rf ./src/var/cache/*
+sudo chown $UID:$GID .
+
+docker-compose up -d nginx php-fpm mysql redis
+
+echo "Waiting for mysql startup..."
+( docker logs -f burger-king_mysql 2>&1 & ) | grep -q "MySQL init process done. Ready for start up."
+( docker logs -f burger-king_mysql 2>&1 & ) | grep -q "mysqld: ready for connections."
+echo "Mysql init complete"
+
+docker exec -it -e COMPOSER_MEMORY_LIMIT=-1 my-project_php-fpm composer install --no-interaction --optimize-autoloader --prefer-dist
