3e93bec453f265efe0a3b41335436abe5b7bd636
Alex Borisov <borisov@aeroidea.ru>
2021-03-08 00:26:16 +0300

    fixes

--- build.sh
+++ build.sh
@@ -10,9 +10,4 @@ sudo chown $UID:$GID .
 
 docker-compose up -d nginx php-fpm mysql redis
 
-echo "Waiting for mysql startup..."
-( docker logs -f my-project_mysql 2>&1 & ) | grep -q "MySQL init process done. Ready for start up."
-( docker logs -f my-project_mysql 2>&1 & ) | grep -q "mysqld: ready for connections."
-echo "Mysql init complete"
-
 docker exec -it -e COMPOSER_MEMORY_LIMIT=-1 my-project_php-fpm composer install --no-interaction --optimize-autoloader --prefer-dist
